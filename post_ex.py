279535ce35829592476264a76881bfee506b9051
Kapiloten <kinandafadi@gmail.com>
2026-01-30 11:42:04 +0100

    Contraintes de Budget rajoutées

--- post_ex.py
+++ post_ex.py
@@ -4,18 +4,60 @@ import pandas as pd
 # Le solveur CBC
 print(pulp.listSolvers(onlyAvailable=True))
 
-# Les périodes
-T = [1, 2, 3] # 21 périodes 
+# Le prob est à Maximiser
+prob = pulp.LpProblem("Z", pulp.LpMaximize)
 
+# Les constantes du problèmes
+# Le Budget initial
+B = 140
+# Les périodes
+T = [0, 10, 15] # 3 périodes (0 est pour l'équipe initiale)
+# Les événements
+E = [1, 2, 3] # 21 événements
 # Les coureurs
 P = ["p1", "p2", "p3"] # 184 coureurs
+# Les prix de chaque coureurs
+c = {
+    "p1" : 100
+}
 
-# Variables de décision
-v = pulp.LpVariable.dicts("v", (P, T), cat=pulp.LpBinary)    # 0/1
+## Variables de décision
+# nombre de points gagnés par un coureur p pour une période t
+v = pulp.LpVariable.dicts("v", (P, E), cat=pulp.LpBinary)    # 0/1
+# le budget restant r à la période t
 r = pulp.LpVariable.dicts("r", T, lowBound=0) # >=0
+# le choix d'un coureur p à la période t
+x = pulp.LpVariable.dicts("x", (P, T), cat=pulp.LpBinary)
+# le coureur p entre en jeu à la période t (Transfert) # à implémenter plus tard !
+z = pulp.LpVariable.dicts("z", (P, T), cat=pulp.LpBinary)
+# le coureur p a marqué des points à l'événement e
+y = pulp.LpVariable.dicts("y", (P, E), cat=pulp.Lp.Binary)
 
 
-pulp.lpSum(v[p][t] for p in P for t in T)
+# Listes de contraintes
+# Budget Dyn # Les coûts ne changent pas ?
+for i in range(1, len(T)):
+    t_1 = T[i-1]
+    t = T[i]
+    prob += (pulp.lpSum(c[p][t] * (x[p][t] - x[p][t_1]) for p in P) == r[t_1] - r[t]), f"Budget_de_{t_1}_à_{t}"
+
+# Attributions des points
+def period(e):
+    if e <= 10:
+        return 0
+    elif e <= 15:
+        return 10
+    else:
+        return 15
+for e in E:
+    t = period(e)
+    for p in P:
+        prob += y[p][e] <= x[p][t], f"{p}_A_marque_à_{e}"
+
+# Budget Initial
+prob += pulp.lpSum(c[p] * x[p][0] for p in P) + r[0] == B, "Budget_Initial"
+
+# pulp.lpSum(v[p][t] for p in P for t in T)
 
 
 """ Somme ≤ variable doublement indexée
