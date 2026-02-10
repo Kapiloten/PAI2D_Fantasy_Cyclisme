1832dab74783f40dd876ea7e9261e060710d2baf
Kapiloten <kinandafadi@gmail.com>
2026-02-10 18:47:51 +0100

    Modèle avant transfert.

--- post_ex.py
+++ post_ex.py
@@ -1,5 +1,8 @@
 import pulp
 import pandas as pd
+import trait_donnees
+
+points, E_final = trait_donnees.data_tour_de_france()
 
 
 def period(e):
@@ -17,6 +20,7 @@ print(pulp.listSolvers(onlyAvailable=True))
 # Le prob est à Maximiser
 prob = pulp.LpProblem("Z", pulp.LpMaximize)
 
+
 # Les constantes du problèmes
 # Le Budget initial
 B = 140
@@ -25,11 +29,196 @@ T = [0, 10, 15] # 3 périodes (0 est pour l'équipe initiale)
 # Les événements
 E = list(range(1, 22)) # 21 événements
 # Les coureurs
-P = ["p1", "p2", "p3"] # 184 coureurs
+P = ['Philipsen Jasper', 'Groves Kaden', 'Poel Mathieu', 'Rickaert Jonas', 'Meurisse Xandro', 'Vermeersch Gianni', 'Verstrynge Emiel', 'Dillier Silvan', 'Vauquelin Kévin', 'Capiot Amaury', 'Démare Arnaud', 'Venturini Clément', 'Le Berre Mathis', 'Rodr Cristi', 'Garc Pierna Ra', 'Costiou Ewen', 'Stannard Robert', 'Bauhaus Phil', 'Buitrago Santiago', 'Haig Jack', 'Wright Fred', 'Mohori Matej', 'Gradek Kamil', 'Martinez Lenny', 'Touzé Damien', 'Coquard Bryan', 'Buchmann Emanuel', 'Renard Alexis', 'Aranburu Alex', 'Teuns Dylan', 'Izagirre Ion', 'Thomas Benjamin', 'Berthet Clément', 'Paret- Peintre Aurélien', 'Naesen Oliver', 'Gall Felix', 'Tronchon Bastien', 'Scotson Callum', 'Armirail Bruno', 'Bissegger Stefan', 'Sweeny Harry', 'Asgreen Kasper', 'Powless Neilson', 'Healy Ben', 'Valgren Michael', 'Albanese Vincenzo', 'Baudin Alex', 'Berg Marijn', 'Russo Clément', 'Penho Paul', 'Barthe Cyril', 'Madouas Valentin', 'Martin Guillaume', 'Pacher Quentin', 'Grégoire Romain', 'Askey Lewis', 'Watson Samuel', 'Rodr Carlos', 'Swift Connor', 'Thomas Geraint', 'Foss Tobias', 'Arensman Thymen', 'Laurance Axel', 'Ganna Filippo', 'Girmay Biniam', 'Page Hugo', 'Braet Vito', 'Rutsch Jonas', 'Zimmermann Georg', 'Rex Laurenz', 'Sintmaartensdijk Roel', 'Barré Louis', 'Neilands Krists', 'Ackermann Pascal', 'Blackmore Joseph', 'Boivin Guillaume', 'Louvel Matis', 'Stewart Jake', 'Lutsenko Alexey', 'Woods Michael', 'Stuyven Jasper', 'Milan Jonathan', 'Skjelmose Mattias', 'Skuji Toms', 'Theuns Edward', 'Consonni Simone', 'Simmons Quinn', 'Nys Thibau', 'De Lie Arnaud', 'De Buyst Jasper', 'Van Eetvelt Lennert', 'Berckmoes Jenno', 'Van Moer Brent', 'Drizners Jarrad', 'Grignard Sébastien', 'Sep Eduardo', 'Garc Cortina Iv', 'Mas Enric', 'Oliveira Nelson', 'Mühlberger Gregor', 'Rubio Einer', 'Castrillo Pablo', 'Romeo Iv', 'Barta Will', 'Meeus Jordi', 'Poppel Danny', 'Lipowitz Florian', 'Rogli Primo', 'Vlasov Aleksandr', 'Pithie Laurence', 'Moscon Gianni', 'Dijke Mick', 'Merlier Tim', 'Evenepoel Remco', 'Van Wilder Ilan', 'Eenkhoorn Pascal', 'Van Lerberghe Bert', 'Cattaneo Mattia', 'Paret- Peintre Valentin', 'Schachmann Maximilian', 'Mezgec Luka', 'Groenewegen Dylan', 'Reinders Elmar', 'Dunbar Eddie', 'Connor Ben', 'Schmid Mauro', 'Durbridge Luke', 'Plapp Luke', 'Märkl Niklas', 'Bittner Pavel', 'Andresen Tobias Lund', 'Onley Oscar', 'Flynn Sean', 'Barguil Warren', 'Broek Frank', 'Naberman Tim', 'Turgis Anthony', 'Jegat Jordan', 'Gachignard Thomas', 'Cras Steff', 'Delettre Alexandre', 'Jeannière Emilien', 'Burgaudeau Mathieu', 'Vercher Mattéo', 'Jorgenson Matteo', 'Vingegaard Jonas', 'Benoot Tiesj', 'Affini Edoardo', 'Aert Wout', 'Campenaerts Victor', 'Kuss Sepp', 'Yates Simon', 'Trentin Matteo', 'Mayrhofer Marius', 'Haller Marco', 'Dainese Alberto', 'Lienhard Fabian', 'Hirschi Marc', 'Storer Michael', 'Alaphilippe Julian', 'Poga Tadej', 'Wellens Tim', 'Almeida Jo', 'Narv Jhonatan', 'Soler Marc', 'Politt Nils', 'Sivakov Pavel', 'Yates Adam', 'Wærenskjold Søren', 'Fredheim Stian', 'Johannessen Tobias Halland', 'Abrahamsen Jonas', 'Hoelgaard Markus', 'Cort Magnus', 'Leknessund Andreas', 'Johannessen Anders Halland', 'Teunissen Mike', 'Ballerini Davide', 'Tejada Harold', 'Higuita Sergio', 'Champoussin Clément', 'Velasco Simone', 'Fedorov Yevgeniy', 'Bol Cees'] # 184 coureurs
 # Les prix de chaque coureurs
 c = {
-    "p1" : 100
+    "Philipsen Jasper": 16,
+    "Groves Kaden": 14,
+    "Poel Mathieu": 14,
+    "Rickaert Jonas": 4,
+    "Meurisse Xandro": 6,
+    "Vermeersch Gianni": 8,
+    "Verstrynge Emiel": 4,
+    "Dillier Silvan": 4,
+    "Vauquelin Kévin": 10,
+    "Capiot Amaury": 7,
+    "Démare Arnaud": 10,
+    "Venturini Clément": 6,
+    "Le Berre Mathis": 5,
+    "Rodr Cristi": 7,
+    "Garc Pierna Ra": 5,
+    "Costiou Ewen": 6,
+    "Stannard Robert": 5,
+    "Bauhaus Phil": 12,
+    "Buitrago Santiago": 12,
+    "Haig Jack": 8,
+    "Wright Fred": 7,
+    "Mohori Matej": 9,
+    "Gradek Kamil": 4,
+    "Martinez Lenny": 12,
+    "Touzé Damien": 4,
+    "Coquard Bryan": 10,
+    "Buchmann Emanuel": 8,
+    "Renard Alexis": 6,
+    "Aranburu Alex": 8,
+    "Teuns Dylan": 8,
+    "Izagirre Ion": 8,
+    "Thomas Benjamin": 6,
+    "Berthet Clément": 5,
+    "Paret- Peintre Aurélien": 8,
+    "Naesen Oliver": 6,
+    "Gall Felix": 12,
+    "Tronchon Bastien": 5,
+    "Scotson Callum": 3,
+    "Armirail Bruno": 7,
+    "Bissegger Stefan": 7,
+    "Sweeny Harry": 4,
+    "Asgreen Kasper": 9,
+    "Powless Neilson": 10,
+    "Healy Ben": 11,
+    "Valgren Michael": 6,
+    "Albanese Vincenzo": 5,
+    "Baudin Alex": 4,
+    "Berg Marijn": 9,
+    "Russo Clément": 6,
+    "Penho Paul": 7,
+    "Barthe Cyril": 5,
+    "Madouas Valentin": 10,
+    "Martin Guillaume": 10,
+    "Pacher Quentin": 6,
+    "Grégoire Romain": 10,
+    "Askey Lewis": 7,
+    "Watson Samuel": 7,
+    "Rodr Carlos": 15,
+    "Swift Connor": 5,
+    "Thomas Geraint": 10,
+    "Foss Tobias": 8,
+    "Arensman Thymen": 13,
+    "Laurance Axel": 10,
+    "Ganna Filippo": 12,
+    "Girmay Biniam": 14,
+    "Page Hugo": 7,
+    "Braet Vito": 6,
+    "Rutsch Jonas": 5,
+    "Zimmermann Georg": 6,
+    "Rex Laurenz": 7,
+    "Sintmaartensdijk Roel": 3,
+    "Barré Louis": 7,
+    "Neilands Krists": 7,
+    "Ackermann Pascal": 10,
+    "Blackmore Joseph": 9,
+    "Boivin Guillaume": 4,
+    "Louvel Matis": 4,
+    "Stewart Jake": 5,
+    "Lutsenko Alexey": 9,
+    "Woods Michael": 9,
+    "Stuyven Jasper": 9,
+    "Milan Jonathan": 14,
+    "Skjelmose Mattias": 12,
+    "Skuji Toms": 7,
+    "Theuns Edward": 4,
+    "Consonni Simone": 6,
+    "Simmons Quinn": 4,
+    "Nys Thibau": 11,
+    "De Lie Arnaud": 11,
+    "De Buyst Jasper": 4,
+    "Van Eetvelt Lennert": 11,
+    "Berckmoes Jenno": 6,
+    "Van Moer Brent": 6,
+    "Drizners Jarrad": 3,
+    "Grignard Sébastien": 3,
+    "Sep Eduardo": 4,
+    "Garc Cortina Iv": 7,
+    "Mas Enric": 12,
+    "Oliveira Nelson": 5,
+    "Mühlberger Gregor": 4,
+    "Rubio Einer": 7,
+    "Castrillo Pablo": 9,
+    "Romeo Iv": 5,
+    "Barta Will": 4,
+    "Meeus Jordi": 11,
+    "Poppel Danny": 7,
+    "Lipowitz Florian": 11,
+    "Rogli Primo": 16,
+    "Vlasov Aleksandr": 12,
+    "Pithie Laurence": 7,
+    "Moscon Gianni": 6,
+    "Dijke Mick": 4,
+    "Merlier Tim": 16,
+    "Evenepoel Remco": 18,
+    "Van Wilder Ilan": 9,
+    "Eenkhoorn Pascal": 3,
+    "Van Lerberghe Bert": 4,
+    "Cattaneo Mattia": 7,
+    "Paret- Peintre Valentin": 9,
+    "Schachmann Maximilian": 7,
+    "Mezgec Luka": 7,
+    "Groenewegen Dylan": 13,
+    "Reinders Elmar": 3,
+    "Dunbar Eddie": 10,
+    "Connor Ben": 13,
+    "Schmid Mauro": 7,
+    "Durbridge Luke": 5,
+    "Plapp Luke": 7,
+    "Märkl Niklas": 3,
+    "Bittner Pavel": 8,
+    "Andresen Tobias Lund": 3,
+    "Onley Oscar": 8,
+    "Flynn Sean": 3,
+    "Barguil Warren": 8,
+    "Broek Frank": 6,
+    "Naberman Tim": 3,
+    "Turgis Anthony": 7,
+    "Jegat Jordan": 6,
+    "Gachignard Thomas": 4,
+    "Cras Steff": 9,
+    "Delettre Alexandre": 5,
+    "Jeannière Emilien": 7,
+    "Burgaudeau Mathieu": 6,
+    "Vercher Mattéo": 5,
+    "Jorgenson Matteo": 14,
+    "Vingegaard Jonas": 19,
+    "Benoot Tiesj": 8,
+    "Affini Edoardo": 5,
+    "Aert Wout": 13,
+    "Campenaerts Victor": 8,
+    "Kuss Sepp": 13,
+    "Yates Simon": 13,
+    "Trentin Matteo": 8,
+    "Mayrhofer Marius": 3,
+    "Haller Marco": 6,
+    "Dainese Alberto": 9,
+    "Lienhard Fabian": 3,
+    "Hirschi Marc": 10,
+    "Storer Michael": 10,
+    "Alaphilippe Julian": 10,
+    "Poga Tadej": 20,
+    "Wellens Tim": 9,
+    "Almeida Jo": 15,
+    "Narv Jhonatan": 9,
+    "Soler Marc": 8,
+    "Politt Nils": 7,
+    "Sivakov Pavel": 7,
+    "Yates Adam": 14,
+    "Wærenskjold Søren": 9,
+    "Fredheim Stian": 7,
+    "Johannessen Tobias Halland": 8,
+    "Abrahamsen Jonas": 7,
+    "Hoelgaard Markus": 6,
+    "Cort Magnus": 10,
+    "Leknessund Andreas": 6,
+    "Johannessen Anders Halland": 6,
+    "Teunissen Mike": 7,
+    "Ballerini Davide": 7,
+    "Tejada Harold": 8,
+    "Higuita Sergio": 6,
+    "Champoussin Clément": 8,
+    "Velasco Simone": 6,
+    "Fedorov Yevgeniy": 6,
+    "Bol Cees": 8
 }
+
+
 
 ## Variables de décision
 # le budget restant r à la période t
@@ -38,11 +227,18 @@ r = pulp.LpVariable.dicts("r", T, lowBound=0) # >=0
 x = pulp.LpVariable.dicts("x", (P, T), cat=pulp.LpBinary)
 # le coureur p entre en jeu à la période t (Transfert) # à implémenter plus tard !
 z = pulp.LpVariable.dicts("z", (P, T), cat=pulp.LpBinary)
-# le coureur p a marqué des points à l'événement e
-y = pulp.LpVariable.dicts("y", (P, E), cat=pulp.LpBinary)
 
 # Le prob à maximiser
 # prob += pulp.lpSum(v[p][e] * y[p][e] for p in P for e in E) # à implémenter plus tard !
+prob += pulp.lpSum(
+    points.get((p, e), 0) * x[p][period(e)]
+    for p in P
+    for e in E
+) + pulp.lpSum(
+    points.get((p, E_final), 0) * x[p][15]
+    for p in P
+)
+
 """
 t_fin = 15  
 prob += (
@@ -59,11 +255,6 @@ for i in range(1, len(T)):
     t = T[i]
     prob += (pulp.lpSum(c[p] * (x[p][t] - x[p][t_1]) for p in P) == r[t_1] - r[t]), f"Budget_de_{t_1}_à_{t}"
 
-# Attributions des points
-for e in E:
-    t = period(e)
-    for p in P:
-        prob += y[p][e] <= x[p][t], f"{p}_A_marque_à_{e}"
 
 # Budget Initial
 prob += pulp.lpSum(c[p] * x[p][0] for p in P) + r[0] == B, "Budget_Initial"
@@ -71,6 +262,63 @@ prob += pulp.lpSum(c[p] * x[p][0] for p in P) + r[0] == B, "Budget_Initial"
 # Nombre de coureurs à chaque période
 for t in T:
     prob += pulp.lpSum(x[p][t] for p in P) == 14
+
+
+# Résolution du problème !!
+status = prob.solve(pulp.PULP_CBC_CMD(msg=True))
+# print("Status :", pulp.LpStatus[status])
+
+
+# print("Objectif =", prob.objective)
+expr = prob.objective  # LpAffineExpression
+
+for var, coef in expr.items():
+    pass
+    # print(var.name, coef)
+    # print(len(prob.objective))
+    # print(type(prob.objective))
+
+# print("Nb contraintes =", len(prob.constraints))
+# prob.writeLP("modele_tdf.lp")
+
+# print(pulp.value(x["Vingegaard Jonas"][10]))
+
+def equipe_opt(t):
+    return [p for p in P if pulp.value(x[p][t]) == 1]
+for t in T:
+    team = equipe_opt(t)
+    print(f"\nÉquipe optimale à la période t={t}")
+    for p in team:
+        print(p)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- trait_donnees.py
+++ trait_donnees.py
@@ -24,10 +24,14 @@ points_top_20 = [30, 20, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3
 
 e = 0
 for k in range(3):
-    for j in range(1, 9):
-        if k == 2 and j == 2:
+    for j in range(0, 10):
+        if (k == 0 and j == 0):
+            continue
+        if (k == 2 and j == 2):
+            # print(e)
             break
         e += 1  # étape courante = 1..21
+        # print(k, j, e)
         df_stage = pd.read_csv(r"C:\ROOT\M1_IA_Sorbonne\AI2D Projet\PAI2D\tdf_rankings\stage_" + str(k) + str(j) + "_2025.csv")
         for i in range(min(len(df_stage), 20)):
             coureur = df_stage.loc[i, "Rider"]
@@ -47,7 +51,10 @@ bonus_blanc = 5
 bonus_combatif1 = 5
 bonus_combatif2 = 5
 
-for i in range(len(df_bonus)): 
+
+combatifs = {}
+e = 0
+for i in range(min(len(df_bonus), 21)):
     e = i + 1
 
     for col, b in [("Jaune", bonus_jaune),
@@ -62,6 +69,14 @@ for i in range(len(df_bonus)):
     if pd.notna(coureur2) and coureur2 != "":
         points[(coureur2, e)] = points.get((coureur2, e), 0) + bonus_combatif2
 
+    for col in ["Combatif1", "Combatif2"]:
+        coureur = df_bonus.loc[i, col]
+        if pd.notna(coureur) and coureur != "":
+            combatifs[(coureur)] = combatifs.get((coureur), 0) + 1
+# 10 pour le Super Combatif
+bonus_super_combatif = 10
+super_combatif = max(combatifs, key=combatifs.get)
+# print(super_combatif)
 """
 for i in range(23) : # i+1 jouera le rôle de e
     # Jaune,Vert,Pois,Blanc,Combatif1,Combatif2
@@ -75,10 +90,43 @@ for i in range(23) : # i+1 jouera le rôle de e
 """
 
 # -----------------------------------------------------------------------------------
+# Points du classement final par maillot
 
 df_final = pd.read_csv(r"C:\ROOT\M1_IA_Sorbonne\AI2D Projet\PAI2D\tdf_rankings\final.csv")
+#df_final = pd.read_csv(r"PAI2D_Fantasy_Cyclisme-master/tdf_rankings/final.csv")
 
-for i in range(min(len(df_final), 185)):
-    coureur = df_final.loc[i, "Rider"]
+# étape pour le classement final par maillot
+E_final = 22
+for i in range(len(df_final)):
+    # Nom du cycliste
+    coureur = df_final.loc[i, df_final.columns[0]]
+    points[(coureur, E_final)] = points.get((coureur, E_final), 0)
+
     if pd.notna(coureur) and coureur != "":
-        # points[(coureur, e)] = points.get((coureur, e), 0) + b
+        # Parcours tous les maillots
+        for maillots in df_final.columns[1:]:
+            pts = df_final.loc[i, maillots]
+
+            if pd.notna(pts) and pts != 0:
+                points[(coureur, E_final)] += pts
+
+        points[(coureur, E_final)] = int(points[(coureur, E_final)])
+points[(super_combatif, E_final)] = points.get((super_combatif, E_final), 0) + bonus_super_combatif
+
+        #print("-----------------")
+        #print(coureur)
+        #print(points[(coureur, E_final)])
+        #print("-----------------")
+
+
+# -----------------------------------------------------------------------------------
+
+# df_final = pd.read_csv(r"C:\ROOT\M1_IA_Sorbonne\AI2D Projet\PAI2D\tdf_rankings\final.csv")
+
+# for i in range(min(len(df_final), 185)):
+#     coureur = df_final.loc[i, "Rider"]
+#     if pd.notna(coureur) and coureur != "":
+#         # points[(coureur, e)] = points.get((coureur, e), 0) + b
+
+def data_tour_de_france():
+    return points, E_final
